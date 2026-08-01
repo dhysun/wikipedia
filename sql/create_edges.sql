5aa8c324a88f38f77734bad977181531f03d69eb
Zachary Xiao <zyxiao2007@gmail.com>
2026-07-31 19:31:20 -0600

    Update create_edges.sql

--- sql/create_edges.sql
+++ sql/create_edges.sql
@@ -7,3 +7,13 @@ CREATE TABLE edges
   to_page_id INT NOT NULL, 
   PRIMARY KEY (from_page_id, to_page_id), INDEX (to_page_id) 
 );
+
+INSERT INTO edges 
+SELECT 
+  pagelinks.pl_from, 
+  page.page_id 
+FROM pagelinks 
+JOIN linktarget
+  ON pagelinks.pl_target_id = linktarget.lt_id 
+JOIN page 
+  ON page.page_title = linktarget.lt_title; 
