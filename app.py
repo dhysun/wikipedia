bc39430b1fb0ad393a8c358ed69ded145628a7a3
dhysun <davidhongyusun@gmail.com>
2026-08-03 16:42:48 -0400

    flask boilerplate pt 2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-# from .bfs import WikiSearch
+import bfs
+import autocomplete
 
 app = Flask(__name__)
 
