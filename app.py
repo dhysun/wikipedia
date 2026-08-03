05288545d41f123be47160352e9a49188f25f467
dhysun <davidhongyusun@gmail.com>
2026-08-03 13:42:05 -0400

    flask boilerplate

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+# from .bfs import WikiSearch
+
+app = Flask(__name__)
+
+app.secret_key = 'your_secret_key'  # Replace
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
